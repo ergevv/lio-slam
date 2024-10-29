88fecd737add5bf07cbff5e058b2cfff6f7c3ca0
ergevv <2822936659@qq.comm>
2024-10-29 18:59:27 +0800

    删除

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/erge/桌面/开源工程/ros_ws/src
-# Build directory: /home/erge/桌面/开源工程/ros_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("slam")
